873602009aa5de405a8c86d3021aa3a9fb8a98ae
George Danezis <g.danezis@ucl.ac.uk>
2017-09-02 15:37:37 +0100

    Added hello World content

--- Topics/01_Basics/src/hello_world.py
+++ Topics/01_Basics/src/hello_world.py
@@ -2,3 +2,7 @@
 
 def hello_world():
     print("Hello World!") 
+
+# run if called as `pyhton hello_world.py'
+if __name__ == "__main__": # Pyhton idiom
+    hello_world()          # `Call' the function `hello_world'
