60c2a603a8ba6bc2d10fcd53e5f4dc1b63d5870e
George Danezis <g.danezis@ucl.ac.uk>
2017-10-01 21:44:08 +0100

    Fixed bugs in example 1-1

--- Topics/01_Basics/src/exercise11.py
+++ Topics/01_Basics/src/exercise11.py
@@ -3,23 +3,30 @@ from math import e
 def NR_ln(v, tolerance = 0.001):
     "Computes the natural logarithm of v up to a precision `tolerence'."
     if v <= 0.0:
-        raise ArithmeticError("Negative logarithms are undefined: %s" % v)
+        raise ArithmeticError("Negative or zero logarithms are undefined: %s" % v)
 
     x_old = v
     expon = e ** x_old
     x = x_old - (expon - v) / expon
-    while not (- tolerance < x_old - x < tolerance): 
+    while not (- tolerance <= x_old - x <= tolerance): 
         x_old = x
         expon = e ** x_old
         x = x_old - (expon - v) / expon
     return x
 
+# ----------- TESTS ------------- #
+
 import pytest
 
 def test_log():
     assert 4.99 < NR_ln(e ** 5) < 5.01
+    assert NR_ln(e ** 5, 0.0) == 5.0
 
 def test_neg():
     with pytest.raises(ArithmeticError) as excinfo:   
         NR_ln(-10.0)
+    assert "Negative" in str(excinfo.value) 
+
+    with pytest.raises(ArithmeticError) as excinfo:   
+        NR_ln(0.0)
     assert "Negative" in str(excinfo.value) 
