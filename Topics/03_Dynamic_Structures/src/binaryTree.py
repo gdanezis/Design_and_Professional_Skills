4c7089ba8ebff5b8254758c9ec902461879edfd8
George Danezis <g.danezis@ucl.ac.uk>
2017-10-30 08:19:52 +0000

    Added trees

--- Topics/03_Dynamic_Structures/src/binaryTree.py
+++ Topics/03_Dynamic_Structures/src/binaryTree.py
@@ -4,29 +4,11 @@ class branch:
     __slots__ = ["item", "left", "right"]
 
     def __init__(self, item, left=None, right=None):
-        """ Initialize a tree branch, with an item and a left / right set of branches """
+        """ Initialize a tree branch, with an item and a 
+            left / right set of branches """
         self.item = item
         self.left = left
         self.right = right
-
-    def len(self):
-        """ Returns the total number of items stored. """
-        L,R = 0,0
-        if self.left is not None:
-            L = self.left.len()
-        if self.right is not None:
-            R = self.right.len()
-        return 1 + L + R
-
-    def depth(self):
-        """ Returns the maximal tree depth. """
-        L, R = 0,0
-        if self.left:
-            L = self.left.depth()
-        if self.right:
-            R = self.right.depth()
-
-        return 1 + max(L, R)
 
     def add(self, item):
         """ Adds an item to the branch. """
@@ -57,32 +39,49 @@ class branch:
         """ Returns a whether the item is in the tree. """
         return self.get(item) is not None
 
-    def min(self):
+
+    def len(self):
+        """ Returns the total number of items stored. """
+        L,R = 0,0
+        if self.left is not None:
+            L = self.left.len()
+        if self.right is not None:
+            R = self.right.len()
+        return 1 + L + R
+
+    def depth(self):
+        """ Returns the maximal tree depth. """
+        L, R = 0,0
+        if self.left:
+            L = self.left.depth()
+        if self.right:
+            R = self.right.depth()
+
+        return 1 + max(L, R)
+
+    def max(self):
         """ Returns the minimum item. """
-        if self.left is None:
+        if self.right is None:
             return self.item
         else:
-            return self.left.min()
+            return self.right.max()
 
     def remove(self, item):
         """ Removes an item, and returns a mutated version of the tree. """
         if self.item == item:
-            ## 4 Cases
-
             if self.left is None and self.right is None:
-                return None
+                return None       # Case 1 - Its a leaf
             if self.left is None and self.right is not None:
-                return self.right
+                return self.right # Case 2 - No left branch
             if self.left is not None and self.right is None:
-                return self.left
-
+                return self.left  # Case 3 - No right branch
+            # Case 4 - Its a full branch
             if self.left is not None and self.right is not None:
-                new_min = self.right.min()
-                new_right = self.right.remove(new_min)
-                self.item = new_min
-                self.right = new_right
+                new_max = self.left.max()
+                new_left = self.left.remove(new_max)
+                self.item, self.left = new_max, new_left                 
                 return self
-        else:
+        else:                     # Recurse down the correct branch
             if (self.left is not None) and item <= self.item:
                 self.left = self.left.remove(item)
             if (self.right is not None) and self.item < item:
@@ -90,7 +89,7 @@ class branch:
             return self
 
     def walk(self):
-        """ Performs a depth first, ordered, search of the tree as an iterator. """
+        """ Performs a depth first traversal of the tree as an iterator. """
         if self.left is not None:
             yield from self.left.walk()
         yield self.item
@@ -100,10 +99,17 @@ class branch:
 
 
 import random
-    
-def test_add_isin():
+import pytest
+
+@pytest.fixture
+def two_lists():
     items = list(range(100))
+    old_items = items.copy()
     random.shuffle(items)
+    return (items, old_items)
+
+def test_add_isin(two_lists):
+    items, _ = two_lists
 
     b = branch(items[0])
     for i in items[1:50]:
@@ -115,35 +121,33 @@ def test_add_isin():
     for i in items[50:]:
         assert not b.isin(i)
 
-def test_min_remove_len():
-    m = list(range(100))
-    random.shuffle(m)
-
-    b = branch(m[0])
-    for i in m[1:]:
-        b.add(i)
-
-    assert b.min() == 0
-
-    L = 100
-    for i in m:
-        assert b.len() == L
-        assert b.isin(i)
-        b = b.remove(i)
+def test_min_remove_len(two_lists):
+    items, _ = two_lists    # Use a test fixture 
+                            # returning range(100)
+    # Test init and add    
+    b = branch(items[0])            # Test init
+    for i in items[1:]:
+        b.add(i)                    # Test add
+    assert b.max() == 99            # Test max
+    # Test remove, isin, len
+    for pos, i in enumerate(items): # Helper iterator
+        assert b.len() == 100 - pos # Test len
+        assert b.isin(i)            # Test isin
+        b = b.remove(i)             # Test remove
         if b != None:
-            assert not b.isin(i)
-        L -= 1
+            assert not b.isin(i)    # Test isin
     assert b == None
 
-def test_walk():
-    m = list(range(100))
-    mc = m.copy()
-    random.shuffle(mc)
+def test_walk(two_lists):
+    items = list(range(100))        # [0, ..., 99]
+    random.shuffle(items)           # shuffle
 
-    b = branch(mc[0])
-    for i in mc[1:]:
+    b = branch(items[0])            # Create tree
+    for i in items[1:]:
         b.add(i)
 
-    assert len(m) == b.len()
-    for x,y in zip(m, b.walk()):
-        assert x == y
+    assert len(items) == b.len()    # Test len
+
+    # Check that iterator returns elements in order
+    for x,y in zip(range(100), b.walk()):
+        assert x == y  # Check the order is the same
--- Topics/03_Dynamic_Structures/src/minimap.py
+++ Topics/03_Dynamic_Structures/src/minimap.py
@@ -1,44 +1,49 @@
 from binaryTree import branch
 
-class mItem():
-    """ A class used to sotre objects within the minimap. """
-
+class KeyValue():
+    """ A class representing a single key-value pair """
     __slots__ = ["key", "value"]
 
     def __init__(self, key, value):
         self.key = key
         self.value = value
 
-    def update(self, value):
+    def update(self, value): # Get the value
         self.value = value
 
-    def __eq__(self, other):
+    def __eq__(self, other):  # The == operator
         return self.key == other.key
 
-    def __lt__(self, other):
+    def __lt__(self, other):  # The < operator
         return self.key < other.key
 
-    def __le__(self, other):
+    def __le__(self, other):  # The <= operator
         return self.key <= other.key
 
 
 class minimap():
-
-    __slots__ = [ "root" ]
+    __slots__ = [ "root" ] # Efficient storage.
 
     def __init__(self):
         """ Creates an empty minimap object. """
         self.root = None
 
-    def __len__ (self):
-        """ Returns the number of key-value pairs stored. """
-        if self.root:
-            return self.root.len()
-        return 0
+    def __setitem__ (self, key, value):
+        """ Implements assignment: M[k] = v """
+        template = KeyValue(key, value) # The new KV item
+        if self.root is None:           # Create a new tree
+            self.root = branch(template)
+
+        else:
+            match = self.root.get(template)
+            if match is None:           # Add the KV item
+                self.root.add(template)
+            else:                       # Update exiting KV
+                match.update(value)
 
     def __getitem__ (self, key):
         """ Implements key lookup: M[k] """
-        template = mItem(key, None)
+        template = KeyValue(key, None)
         if self.root is None:
             raise IndexError()
         
@@ -48,24 +53,11 @@ class minimap():
 
         return item.value
 
-    def __setitem__ (self, key, value):
-        """ Implements assignment: M[k] = v """
-        template = mItem(key, value)
-        if self.root is None:
-            self.root = branch(template)
-
-        else:
-            match = self.root.get(template)
-            if match is None:
-                self.root.add(template)
-            else:
-                match.update(value)
-
     def __delitem__ (self, key):
         """ Implements the del operator. """
         if self.root is None:
             return
-        template = mItem(key, None)
+        template = KeyValue(key, None)
         self.root = self.root.remove(template)
 
     def __contains__ (self, key):
@@ -73,7 +65,7 @@ class minimap():
         if self.root is None:
             return False
 
-        template = mItem(key, None)
+        template = KeyValue(key, None)
         return self.root.isin(template)
 
     def __iter__(self):
@@ -84,12 +76,19 @@ class minimap():
             for item in self.root.walk():
                 yield item.key
 
+    def __len__ (self):
+        """ Returns the number of key-value pairs stored. """
+        if self.root:
+            return self.root.len()
+        return 0
+
+## Must be LINE 85
 import pytest
 
 def test_item():
-    m21 = mItem(2, 1)
-    m23 = mItem(2, 3)
-    m33 = mItem(3, 3)
+    m21 = KeyValue(2, 1)
+    m23 = KeyValue(2, 3)
+    m33 = KeyValue(3, 3)
     assert m21 == m23
     assert m33 > m23
     assert m21 < m33
@@ -100,10 +99,20 @@ def test_item():
 
 @pytest.fixture
 def map_sample():
-    M = minimap()
+    M = minimap()  
     K,V = list(range(10)), list(range(0,100, 10))
     V2 = list(range(0,1000, 100))
     return (M, K, V, V2)    
+
+def test_key_value_operations():
+    phonebook = minimap()  # A simple mapping type
+    phonebook["Alice"] = "07877654324"
+    phonebook["bob"]   = "07764836211"
+
+    number = phonebook["Alice"]
+    assert number == "07877654324"
+    assert len(phonebook) == 2
+
 
 def test_map_assign(map_sample):
     (M, K, V, V2) = map_sample
@@ -111,8 +120,8 @@ def test_map_assign(map_sample):
     for k, v in zip(K,V):
         M[k] = v
 
-def test_map_get(map_sample):
-    (M, K, V, V2) = map_sample
+def test_map_set_get(map_sample):
+    (M, K, V, _) = map_sample
 
     for k, v in zip(K,V):
         M[k] = v
